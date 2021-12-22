d5bfed1ad49078e7dd2f401c979bce866235da8b
SamettEce <96135911+SamettEce@users.noreply.github.com>
2021-12-23 01:34:22 +0300

    komut

---
+++ Samet Ece/Helix Jump/Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb; // top sürekli aşağı düşerken yukarı doğru zıplaması için rigid body kullanıyoruz
+    [SerializeField] private GameObject splashImg;
+    [SerializeField] private float jumpForce;
+    private GameManager gm;
+    private Ring ring;
+    
+    void Start()
+    {
+        gm = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    private void OnCollisionEnter(Collision other) { // bir objeye çarptığı zaman 
+        rb.AddForce(Vector3.up * jumpForce); // yukarı doğru zıpla
+        GameObject splash =  Instantiate(splashImg, transform.position - new Vector3(0, 0.22f, 0f), transform.rotation);// topun konumunu ayarlıyoruz
+        splash.transform.SetParent(other.gameObject.transform); //splash konumu carptığımız objenin formuna
+        Destroy(splash, 1);
+
+
+        //çaprtığı objenin metaryel adını alır
+        string metarialName = other.gameObject.GetComponent<MeshRenderer>().material.name;
+        Debug.Log("Metaryel Adı: " + metarialName);
+        if(metarialName == "Unsafe Color (Instance)") {
+            gm.restartGame();
+        }
+       
+    }
+}
---
+++ Samet Ece/Helix Jump/Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+    public Transform target; // topumuzun transformu
+    private Vector3 offset;  
+    public float smoothSpeed;
+    void Start()
+    {
+        offset = transform.position - target.position; // kameranın pozisyonundan topun pozisyonunu çıkartalım
+    }
+    void Update()
+    {
+        // lerp methodu iki nokta arasını istediğimiz gibi doldurma biz doldurma olarak değil geçiş efekti olarak kullanacağız
+        Vector3 newPos = Vector3.Lerp(transform.position, offset + target.position, smoothSpeed);
+        transform.position = newPos;   
+
+    }
+}
